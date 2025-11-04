b78efea2be41469037ba1f29b6bf7bf97927e04b
pdmurray <peynmurray@gmail.com>
2025-11-04 00:04:58 -0800

    Handle bad reads on TempFilNum

--- src/model/w2_model_source/Diagenesis Input Files Read 01.f90
+++ src/model/w2_model_source/Diagenesis Input Files Read 01.f90
@@ -29,17 +29,26 @@ Subroutine ReadBedConsolidationFiles(TempFilNum)
     Use CEMAVars
 
     Logical    :: SkipLoop
+    Integer :: ios
     Integer(4) :: TempFilNum
     Real(8) :: TimeJD1
     Real(8) :: TimeJD2
     Real(8) :: FactorInterp
 	Real(8) :: ConsolidRateTemp11(NumConsolidRegns),ConsolidRateTemp1(NumConsolidRegns),ConsolidRateTemp2(NumConsolidRegns)
+    Character(:), allocatable :: fmt
 
     !Read Data
 	SkipLoop = .FALSE.
+    fmt = '(F8.0'//Repeat(',F8.0', NumConsolidRegns)//')'
+
 	Do While(.NOT. SkipLoop .or. EOF(TempFilNum))
-	  Read(TempFilNum,'(F8.0,<NumConsolidRegns>F8.0)')TimeJD1, (ConsolidRateTemp1(i),i=1,NumConsolidRegns)
-	  Read(TempFilNum,'(F8.0,<NumConsolidRegns>F8.0)')TimeJD2, (ConsolidRateTemp2(i),i=1,NumConsolidRegns)
+        Read(TempFilNum, fmt, ios=ios) TimeJD1, (ConsolidRateTemp1(i), i=1, NumConsolidRegns)
+        if (is_iostat_end(ios)) exit
+        if (ios /= 0) stop "Error reading TimeJD1 from TempFilNum"
+
+        Read(TempFilNum, fmt, ios=ios) TimeJD2, (ConsolidRateTemp2(i), i=1, NumConsolidRegns)
+        if (is_iostat_end(ios)) exit
+        if (ios /= 0) stop "Error reading TimeJD2 from TempFilNum"
 
 	    If(JDay >= TimeJD1 .and. JDay <= TimeJD2)then
 		    SkipLoop = .TRUE.
